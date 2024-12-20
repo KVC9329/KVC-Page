f379b9d39e5032b2a63e4e29becfa727421c801f
KVC9329 <kvcconsultantsltd.9329@gmail.com>
2024-12-20 10:12:56 +0000

    updated about page

--- next-js-front/src/app/about/page.jsx
+++ next-js-front/src/app/about/page.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const about = () => {
+const About = () => {
   return (
-    <>
     <div className="bg-gradient-to-b from-green-800 to-black text-white px-4 sm:px-20 py-10">
       <h1 className="text-xl sm:text-2xl leading-relaxed mt-12">
         <span className="font-bold text-white">KVC CONSULTANTS LTD :</span> headquartered in Lucknow is a leading end-to-end human resource service company in India which provides temporary, permanent, and contract staffing solutions to companies. Started in 2000, the company has clocked a steady growth overcoming stiff competition and challenges to emerge as pioneers in servicing our clients.
@@ -12,7 +11,7 @@ const about = () => {
         <span className="font-bold text-white">KVC CONSULTANTS LTD :</span> is a brainchild of qualified professionals, each with over 15 years of rich and diversified corporate exposure in fields spanning sales, customer service, project management, financial markets, education, learning & development.
       </h1>
       <br />
-      <p className="text-lg leading-relaxed ">
+      <p className="text-lg leading-relaxed">
         We have a vintage of more than fifteen years in servicing satisfied clients.
         <br />
         Today, KVC CONSULTANTS LTD. operates in the following business lines:
@@ -25,11 +24,10 @@ const about = () => {
       </ul>
       <br />
       <p className="text-lg leading-relaxed ">
-        It's a journey that over time has seen the company grow from strength to strength and emerge as a top-class service provider to meet the growing business needs.
+        It&#39;s a journey that over time has seen the company grow from strength to strength and emerge as a top-class service provider to meet the growing business needs.
       </p>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default about
+export default About;
